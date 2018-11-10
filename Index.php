f18776eab85f6bd17b6e0655dbbeea5005f6af5b
Ray Mercedes <contact@RayMercedes.com>
2018-11-10 03:15:03 -0500

    added slider

--- Index.php
+++ Index.php
@@ -22,224 +22,229 @@
     </div>
 
     <div id="body">
-<!-- Start WOWSlider.com BODY section -->
-<div id="wowslider-container1">
-<div class="ws_images"><ul>
-		<li><a href="#devicetable" target="_self"><img src="data1/images/banner.jpg" alt="Repairs" title="Repairs" id="wows1_0"/></a></li>
-		<li><a href="#services"><img src="data1/images/repairs.jpg" alt="Services" title="Services" id="wows1_1"/></a></li>
-	</ul></div>
-	<div class="ws_bullets"><div>
-		<a href="#" title="Repairs"><span><img src="data1/tooltips/banner.jpg" alt="Repairs"/>1</span></a>
-		<a href="#" title="Services"><span><img src="data1/tooltips/repairs.jpg" alt="Services"/>2</span></a>
-	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">responsive slider</a> by WOWSlider.com v8.8</div>
-<div class="ws_shadow"></div>
-</div>	
-<script type="text/javascript" src="engine1/wowslider.js"></script>
-<script type="text/javascript" src="engine1/script.js"></script>
-<!-- End WOWSlider.com BODY section -->            <div id="devicetitle">
-                <h2>Choose your device below to get started!</h2>
+        <!-- Start WOWSlider.com BODY section -->
+        <div id="wowslider-container1">
+            <div class="ws_images">
+                <ul>
+                    <li><a href="#devicetable" target="_self"><img src="data1/images/banner.jpg" alt="Repairs" title="Repairs" id="wows1_0" /></a></li>
+                    <li><a href="#services"><img src="data1/images/repairs.jpg" alt="Services" title="Services" id="wows1_1" /></a></li>
+                </ul>
             </div>
+            <div class="ws_bullets">
+                <div>
+                    <a href="#" title="Repairs"><span><img src="data1/tooltips/banner.jpg" alt="Repairs" />1</span></a>
+                    <a href="#" title="Services"><span><img src="data1/tooltips/repairs.jpg" alt="Services" />2</span></a>
+                </div>
+            </div>
+            <div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">responsive slider</a> by WOWSlider.com v8.8</div>
+            <div class="ws_shadow"></div>
+        </div>
+        <script type="text/javascript" src="engine1/wowslider.js"></script>
+        <script type="text/javascript" src="engine1/script.js"></script>
+        <!-- End WOWSlider.com BODY section -->
+        <h2>Choose your device below to get started!</h2>
+    </div>
 
-            <div id="devicetable">
-                <table>
-                    <thead>
+    <div id="devicetable">
+        <table>
+            <thead>
 
-                        <th>
-                            <p>Smartphone</p>
-                        </th>
+                <th>
+                    <p>Smartphone</p>
+                </th>
 
-                        <th>
-                            <p>Tablet</p>
-                        </th>
-                        <th>
-                            <p>Computer</p>
-                        </th>
+                <th>
+                    <p>Tablet</p>
+                </th>
+                <th>
+                    <p>Computer</p>
+                </th>
 
-                        <th>
-                            <p>Game Console</p>
-                        </th>
-                    </thead>
+                <th>
+                    <p>Game Console</p>
+                </th>
+            </thead>
 
-                    <tr>
-                        <td>
+            <tr>
+                <td>
 
-                            <div id="smartphones" onclick="smartphonesFunction()">
-                                <img src="Images/Smartphones.png">
-                            </div>
-                        </td>
-
-
-                        <td>
-                            <img src="Images/iPad.png">
-                        </td>
+                    <div id="smartphones" onclick="smartphonesFunction()">
+                        <img src="Images/Smartphones.png">
+                    </div>
+                </td>
 
 
+                <td>
+                    <img src="Images/iPad.png">
+                </td>
 
-                        <td>
-                            <img src="Images/iMac.png">
 
-                            <!--  <div id="selectdevice">
+
+                <td>
+                    <img src="Images/iMac.png">
+
+                    <!--  <div id="selectdevice">
                             <div id="devicebg">
                             </div>
                         </div>
 -->
-                        </td>
-                        <td>
-                            <img src="Images/control.png">
-                        </td>
+                </td>
+                <td>
+                    <img src="Images/control.png">
+                </td>
 
-                    </tr>
-                </table>
-            </div>
+            </tr>
+        </table>
+    </div>
 
-            <!-- start custom code -->
+    <!-- start custom code -->
 
-            <div id="smartphonestable" style="display:none;text-align:center;">
-                <h1>Choose your type of Smartphone!</h1>
-                <table>
-                    <thead>
+    <div id="smartphonestable" style="display:none;text-align:center;">
+        <h1>Choose your type of Smartphone!</h1>
+        <table>
+            <thead>
 
-                        <th>
-                            <p>iPhone</p>
-                        </th>
+                <th>
+                    <p>iPhone</p>
+                </th>
 
-                        <th>
-                            <p>Samsung</p>
+                <th>
+                    <p>Samsung</p>
 
-                        </th>
-                        <th>
-                            <p>Other Smartphone</p>
-                        </th>
-                    </thead>
+                </th>
+                <th>
+                    <p>Other Smartphone</p>
+                </th>
+            </thead>
 
-                    <tr>
-                        <td>
+            <tr>
+                <td>
 
-                            <img src="Images/Smartphones.png" onclick="iphonesFunction()">
-                        </td>
-
-
-                        <td>
-                            <img src="Images/samsung.png">
-                        </td>
+                    <img src="Images/Smartphones.png" onclick="iphonesFunction()">
+                </td>
 
 
-
-                        <td>
-                            <img src="Images/other-smartphone.png">
-
-
-                    </tr>
-                </table>
-
-                <!-- end custom code -->
-
-            </div>
-
-            <!-- start iphone table -->
-
-            <div id="iphonestable" style="display:none;text-align:center;">
-                <h1>Choose your iPhone model!</h1>
-                <table>
-                    <thead>
-
-                        <th>
-                            <p>iPhone 5/5s/5c</p>
-                        </th>
-
-                        <th>
-                            <p>iPhone 6/6s/6+</p>
-
-                        </th>
-                        <th>
-                            <p>iPhone 7/7+</p>
-                        </th>
-                        <th>
-                            <p>iPhone 8/8+</p>
-                        </th>
-                        <th>
-                            <p>iPhone X/Xs</p>
-                        </th>
-
-                    </thead>
-
-                    <tr>
-                        <td>
-
-                            <img src="Images/Smartphones.png">
-                        </td>
-
-
-                        <td>
-                            <img src="Images/samsung.png">
-                        </td>
+                <td>
+                    <img src="Images/samsung.png">
+                </td>
 
 
 
-                        <td>
-                            <img src="Images/other-smartphone.png">
-                        </td>
-
-                        <td>
-                            <img src="Images/other-smartphone.png">
-                        </td>
-
-                        <td>
-                            <img src="Images/other-smartphone.png">
-                        </td>
+                <td>
+                    <img src="Images/other-smartphone.png">
 
 
-                    </tr>
-                </table>
-            </div>
+            </tr>
+        </table>
 
-            <div id="startoverbutton">
-                <button onclick="startOver()"> Start Over</button>
-            </div>
+        <!-- end custom code -->
+
+    </div>
+
+    <!-- start iphone table -->
+
+    <div id="iphonestable" style="display:none;text-align:center;">
+        <h1>Choose your iPhone model!</h1>
+        <table>
+            <thead>
+
+                <th>
+                    <p>iPhone 5/5s/5c</p>
+                </th>
+
+                <th>
+                    <p>iPhone 6/6s/6+</p>
+
+                </th>
+                <th>
+                    <p>iPhone 7/7+</p>
+                </th>
+                <th>
+                    <p>iPhone 8/8+</p>
+                </th>
+                <th>
+                    <p>iPhone X/Xs</p>
+                </th>
+
+            </thead>
+
+            <tr>
+                <td>
+
+                    <img src="Images/Smartphones.png">
+                </td>
+
+
+                <td>
+                    <img src="Images/samsung.png">
+                </td>
+
+
+
+                <td>
+                    <img src="Images/other-smartphone.png">
+                </td>
+
+                <td>
+                    <img src="Images/other-smartphone.png">
+                </td>
+
+                <td>
+                    <img src="Images/other-smartphone.png">
+                </td>
+
+
+            </tr>
+        </table>
+    </div>
+
+    <div id="startoverbutton">
+        <button onclick="startOver()"> Start Over</button>
+    </div>
+    </div>
+
+    <!-- end iphone table -->
+
+    <!-- start services table -->
+
+
+    <div id="services">
+        <h1> What we can help with!</h1>
+        <div id="servicestable">
+            <table>
+                <thead>
+                </thead>
+
+
+                <tr>
+                    <td>
+                        <p>Repair Services</p>
+
+                        <ul>
+                            <li>Broken Screen</li> -
+                            <li>Broken Home Button</li> -
+                            <li>Battery Replacement</li> -
+                            <li>Charger Port Repair</li> -
+                            <li>Water Damage</li> -
+                            <li>No Sound</li> -
+                            <li> And More!</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+
+                        <img src="Images/banner.jpg">
+                    </td>
+
+                </tr>
+            </table>
+
         </div>
-
-        <!-- end iphone table -->
-
-        <!-- start services table -->
-
-
-        <div id="services">
-            <h1> What we can help with!</h1>
-            <div id="servicestable">
-                <table>
-                    <thead>
-                    </thead>
-
-
-                    <tr>
-                        <td>
-                            <p>Repair Services</p>
-
-                            <ul>
-                                <li>Broken Screen</li> -
-                                <li>Broken Home Button</li> -
-                                <li>Battery Replacement</li> -
-                                <li>Charger Port Repair</li> -
-                                <li>Water Damage</li> -
-                                <li>No Sound</li> -
-                                <li> And More!</li>
-                            </ul>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-
-                            <img src="Images/banner.jpg">
-                        </td>
-
-                    </tr>
-                </table>
-
-            </div>
-            <a href="#appointment">
-                <button> Schedule an Appointment or FREE Quote Now!</button>
-            </a>
-        </div>
+        <a href="#appointment">
+            <button> Schedule an Appointment or FREE Quote Now!</button>
+        </a>
+    </div>
     </div>
 
     <div id="appointment">
